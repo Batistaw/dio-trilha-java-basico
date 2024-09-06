759a29562fdb8a1b4393d135dcd602966e84ff4c
Batistaw <wgoncalvesbatistaa@gmail.com>
2024-09-06 00:53:15 -0400

    meu primeiro commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+  
 public class ContaTerminal {
+  public static void main (String [] args ) {
+    double saldo = 500.50; 
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, digite seu nome:"); 
+        String nomeCliente = scanner.next(); 
+        System.out.println("Por favor, digite o numero da agencia:"); 
+        String numeroAgencia = scanner.next();
+        System.out.println("Por favor, digite o numero da sua conta:");
+        int numeroConta = scanner.nextInt();
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        System.out.println("\"Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência "+ numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque\".");
+    
+  }
+
 }
